64e7f37a467b90212e97021a9e70b8dd9ab1a363
Yaroslav Parfenov <mailpost336@gmail.com>
2026-09-14 13:48:10 +0300

    readme update

--- parfenov.yaroslav/lab3/solution.js
+++ parfenov.yaroslav/lab3/solution.js
@@ -17,9 +17,9 @@ export function sortByFrequency(arr) {
 
   freqElemPairs.sort((a, b) => {
     if (a[0] < b[0]) {
-      return -1;
-    } else if (a[0] > b[0]) {
       return 1;
+    } else if (a[0] > b[0]) {
+      return -1;
     }
 
     return 0;
