e8f0649488780dcf22aaa0100e7f434c85cd2019
Yaroslav Parfenov <mailpost336@gmail.com>
2026-09-14 11:14:51 +0300

    lab2 init commit

---
+++ parfenov.yaroslav/lab2/solution.js
@@ -0,0 +1,39 @@
+export class ZodiacSign {
+  constructor (name, leftDateLimit, rightDateLimit) {
+    this.name = name;
+    this.leftDateLimit = leftDateLimit;
+    this.rightDateLimit = rightDateLimit;
+  }
+
+  static getAllZodiacSigns(year) {
+    return [
+      new ZodiacSign("Aquarius", Date.parse(year + "-01-20"), Date.parse(year + "-02-18")),
+      new ZodiacSign("Pisces", Date.parse(year + "-02-19"), Date.parse(year + "-03-20")),
+      new ZodiacSign("Aries", Date.parse(year + "-03-21"), Date.parse(year + "-04-19")),
+      new ZodiacSign("Taurus", Date.parse(year + "-04-20"), Date.parse(year + "-05-20")),
+      new ZodiacSign("Gemini", Date.parse(year + "-05-21"), Date.parse(year + "-06-20")),
+      new ZodiacSign("Cancer", Date.parse(year + "-06-21"), Date.parse(year + "-07-22")),
+      new ZodiacSign("Leo", Date.parse(year + "-07-23"), Date.parse(year + "-08-22")),
+      new ZodiacSign("Virgo", Date.parse(year + "-08-23"), Date.parse(year + "-09-22")),
+      new ZodiacSign("Libra", Date.parse(year + "-09-23"), Date.parse(year + "-10-22")),
+      new ZodiacSign("Scorpio", Date.parse(year + "-10-23"), Date.parse(year + "-11-21")),
+      new ZodiacSign("Sagittarius", Date.parse(year + "-11-22"), Date.parse(year + "-12-21")),
+      new ZodiacSign("Capricorn", Date.parse(year + "-12-22"), Date.parse(year + "-12-31")),
+      new ZodiacSign("Capricorn", Date.parse(year + "-01-01"), Date.parse(year + "-01-19")),
+    ];
+  }
+}
+
+export function showZodiacSign(dateString) {
+  let date = new Date(dateString);
+  let year = date.getFullYear();
+
+  const allZodiacSigns = ZodiacSign.getAllZodiacSigns(year);
+  for (let i = 0; i < allZodiacSigns.length; i++) {
+    if (allZodiacSigns[i].leftDateLimit <= date && date <= allZodiacSigns[i].rightDateLimit) {
+      console.log("Your zodiac sign is:", allZodiacSigns[i].name);
+
+      return;
+    }
+  }
+}
